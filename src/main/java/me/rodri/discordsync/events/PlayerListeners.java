64ca9d815cdb249c3860e487b0b73fd4a15eac21
rodri nube <rodrigonuberuiz@gmail.com>
2023-03-21 20:13:54 -0600

    feat: added player death event

--- src/main/java/me/rodri/discordsync/events/PlayerListeners.java
+++ src/main/java/me/rodri/discordsync/events/PlayerListeners.java
@@ -5,6 +5,7 @@ import me.rodri.discordsync.DiscordWebhook;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -35,7 +36,7 @@ public class PlayerListeners implements Listener {
         Player player = event.getPlayer();
 
         DiscordWebhook webhook = new DiscordWebhook(DiscordSync.webhookurl);
-        webhook.addEmbed(new DiscordWebhook.EmbedObject().setColor(Color.red).setTitle("Player leave").setDescription(" has left the server"));
+        webhook.addEmbed(new DiscordWebhook.EmbedObject().setColor(Color.red).setTitle("Player leave").setDescription(player.getDisplayName() + " has left the server"));
 
         try {
             webhook.execute();
@@ -43,6 +44,23 @@ public class PlayerListeners implements Listener {
         catch (java.io.IOException e){
             getLogger().severe(e.getStackTrace().toString());
         }
+
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event){
+        Player player = event.getEntity().getPlayer();
+
+        DiscordWebhook webhook = new DiscordWebhook(DiscordSync.webhookurl);
+        webhook.addEmbed(new DiscordWebhook.EmbedObject().setColor(Color.orange).setTitle("Player death").setDescription(event.getDeathMessage()));
+
+        try {
+            webhook.execute();
+        }
+        catch (java.io.IOException e){
+            getLogger().severe(e.getStackTrace().toString());
+        }
+
 
     }
 
