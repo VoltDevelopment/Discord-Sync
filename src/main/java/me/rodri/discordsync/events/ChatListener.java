e59485114a57ac93ab9b2ecd0da43cc4fdd8414b
rodri nube <rodrigonuberuiz@gmail.com>
2023-03-26 16:07:39 -0600

    feat: new chat listener

---
+++ src/main/java/me/rodri/discordsync/events/ChatListener.java
@@ -0,0 +1,36 @@
+package me.rodri.discordsync.events;
+
+import com.avaje.ebeaninternal.server.cluster.mcast.Message;
+import me.rodri.discordsync.DiscordSync;
+import me.rodri.discordsync.DiscordWebhook;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.awt.*;
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class ChatListener implements Listener {
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event){
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        DiscordWebhook webhook = new DiscordWebhook(DiscordSync.webhookurl);
+        webhook.addEmbed(new DiscordWebhook.EmbedObject().setColor(Color.red).setTitle("New chat message").setDescription(player.getDisplayName() +  "" + message));
+
+        try {
+            webhook.execute();
+        }
+        catch (java.io.IOException e){
+            getLogger().severe(e.getStackTrace().toString());
+        }
+
+
+
+    }
+
+}
