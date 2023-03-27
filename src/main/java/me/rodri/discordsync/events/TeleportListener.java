3a9bb0099a79007ace81260f849deaaa38d82d0a
rodri nube <rodrigonuberuiz@gmail.com>
2023-03-26 19:11:13 -0600

    feat: changed color + testing some strings lmao

--- src/main/java/me/rodri/discordsync/events/TeleportListener.java
+++ src/main/java/me/rodri/discordsync/events/TeleportListener.java
@@ -16,21 +16,29 @@ import static org.bukkit.Bukkit.getLogger;
 public class TeleportListener implements Listener {
 
     @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event){
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
         Player player = event.getPlayer();
         Location location1 = event.getFrom();
         Location location2 = event.getTo();
         PlayerTeleportEvent.TeleportCause cause = event.getCause();
 
+        String fromLocationString = String.format("World: %s" + "X: %.2f  Y: %.2f  Z: %.2f",
+                location1.getWorld().getName(), location1.getX(), location1.getY(), location1.getZ());
+        String toLocationString = String.format("World: %s" + "X: %.2f  Y: %.2f  Z: %.2f",
+                location2.getWorld().getName(), location2.getX(), location2.getY(), location2.getZ());
+        String tpstring =  "" + player.getDisplayName() + " teleport info " + "From: " + fromLocationString + " To: " + toLocationString;
 
         DiscordWebhook webhook = new DiscordWebhook(DiscordSync.webhookurl);
-        webhook.addEmbed(new EmbedObject().setColor(Color.MAGENTA).setTitle("New teleport").setDescription(player.getDisplayName() + " teleport info").addField("**From**", "" + location1, true).addField("**To**", "" + location2, true).addField("**Reason**", "" + cause, false));
+        webhook.addEmbed(new EmbedObject()
+                .setColor(Color.MAGENTA)
+                .setTitle("New teleport")
+                .setDescription(tpstring));
+
         try {
             webhook.execute();
         }
         catch (java.io.IOException e){
             getLogger().severe(e.getStackTrace().toString());
         }
-
     }
 }
